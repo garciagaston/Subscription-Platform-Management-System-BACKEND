709c70bd22776d25b02a97c393d96adac3278abd
Gastón García <gaston.garcia@crowdvac.com>
2024-02-05 16:51:54 -0300

    laravel pint fixes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,5 +21,6 @@ use Illuminate\Routing\Controller as BaseController;
  */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use ValidatesRequests;
 }
