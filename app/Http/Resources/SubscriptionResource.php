b39bbe049f999f11acf872e5348f3085a24539ac
Gastón García <gaston.garcia@crowdvac.com>
2024-02-07 19:08:31 -0300

    improve resources and API documentation

--- app/Http/Resources/SubscriptionResource.php
+++ app/Http/Resources/SubscriptionResource.php
@@ -28,6 +28,7 @@ class SubscriptionResource extends JsonResource
      * @OA\Property(format="string", property="start_date", title="start_date", description="start_date", default="2024-01-01 00:00:00"),
      * @OA\Property(format="string", property="end_date", title="end_date", description="end_date", default="2024-01-01 00:00:00"),
      * @OA\Property(format="boolean", property="active", title="active", description="Package active", default=true),
+     * @OA\Property(property="package", ref="#/components/schemas/PackageResource")
      * @OA\Property(format="string", property="created_at", title="created_at", description="Package created at", default="2024-01-01 00:00:00"),
      * @OA\Property(format="string", property="updated_at", title="updated_at", description="Package updated at", default="2024-01-01 00:00:00"),
      * @OA\Property(format="string", property="deleted_at", title="deleted_at", description="Package deleted at", default="2024-01-01 00:00:00"),
@@ -44,6 +45,7 @@ class SubscriptionResource extends JsonResource
             'start_date' => optional($this->start_date)->toDateTimeString(),
             'end_date' => optional($this->end_date)->toDateTimeString(),
             'active' => $this->active,
+            'package' => (new PackageResource($this->package))->toArray(new Request()),
             'created_at' => optional($this->created_at)->toDateTimeString(),
             'updated_at' => optional($this->updated_at)->toDateTimeString(),
             'deleted_at' => optional($this->deleted_at)->toDateTimeString(),
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -25,6 +25,7 @@ class UserResource extends JsonResource
      * @OA\Property(format="string", property="email", title="email", description="email", default="jon.snow@example.com"),
      * @OA\Property(format="array", property="roles", title="role", description="role", default="[]"),
      * @OA\Property(format="array", property="permissions", title="permissions", description="permissions", default="[]"),
+     * @OA\Property(format="array", type="array", property="subscriptions", description="subscriptions", @OA\Items( ref="#/components/schemas/SubscriptionResource") ),
      * @OA\Property(format="date", property="created_at", title="created_at", description="created_at", default="2024-01-29 15:21:29"),
      * @OA\Property(format="date", property="updated_at", title="updated_at", description="updated_at", default="2024-01-29 15:21:29"),
      * @OA\Property(format="date", property="deleted_at", title="deleted_at", description="deleted_at", default="2024-01-29 15:21:29"),
@@ -47,6 +48,9 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'role' => (array) optional($this->roles->first())->only('id', 'name'),
             'permissions' => count($permissions) ? $permissions : null,
+            'subscriptions' => SubscriptionResource::collection(
+                $this->subscriptions
+            )->toArray(new Request()),
             'created_at' => optional($this->created_at)->toDateTimeString(),
             'updated_at' => optional($this->updated_at)->toDateTimeString(),
             'deleted_at' => optional($this->deleted_at)->toDateTimeString(),
